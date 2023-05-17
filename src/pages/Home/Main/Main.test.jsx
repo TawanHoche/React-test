7da4e3a5e831cf1af5f1af6ebe7dea07625184c7
TawanHoche <123781257+TawanHoche@users.noreply.github.com>
2023-05-17 14:12:34 +0200

    tests léo

--- src/pages/Home/Main/Main.test.jsx
+++ src/pages/Home/Main/Main.test.jsx
@@ -1,46 +1,77 @@
-import React, { useEffect, useState } from 'react'
+import React, { useRef } from 'react'
 import './Main.css'
 
-const Main = ({tasks}) => {
+const Main = ({ TaskData, SelectTask }) => {
+  const taskRefs = useRef([]);
 
+  const handleTaskSelection = (e) => {
+      const TaskId = e.currentTarget;
 
-  const handleSelectTask = (selectedTask) => {
-    if(tasks.map(task => 
-      task.id === selectedTask.id)) {
-        selectedTask.isSelected ? selectedTask.isSelected = false : selectedTask.isSelected = true
-        console.log(selectedTask)
+      if (TaskId.classList.contains('task-selected')) {
+          TaskId.classList.remove('task-selected');
+          SelectTask(null);
+          taskRefs.current.forEach((task) => {
+              task.classList.remove('task-disabled')
+          });
+      } else {
+          taskRefs.current.forEach((task) => {
+              task.classList.remove('task-selected')
+          });
+          TaskId.classList.add('task-selected');
+          SelectTask(TaskId);
+          taskRefs.current.forEach((task) => {
+              task.classList.add('task-disabled')
+          });
+          TaskId.classList.remove('task-disabled');
+          TaskId.classList.add('task-selected');
       }
-  }
+  };
 
-
+  const Task = ({ task, index, handleTaskSelection, taskRef }) => {
+    return (
+        <div
+            className="task"
+            id={`task${index + 1}`}
+            onClick={handleTaskSelection}
+            ref={(element) => (taskRef.current[index] = element)}>
+            <h1>{task.titre}</h1>
+            <p>{task.description}</p>
+            <p>fait ?</p>
+        </div>
+    );
+  };
 
   return (
-    <div className='Main'>
-      <main>
-        <div className="mes_listes">
-        <div className="select">
-          <select>
-              <option>Choisissez votre liste</option>
-              <option>Liste 1</option>
-              <option>Liste 2</option>
-              <option>Ajouter une liste</option>
-          </select>
-        </div>
-        </div>
-        <div className="Tasks">
-          {tasks.map((task) => (
-            <div key={task.id} id={'task ' + task.id}
-            className={`task ${task.isSelected ? 'task-selected' : '' }`}
-            onClick={() => handleSelectTask(task)}>
-              <h1>{task.titre}</h1>
-              <p>{task.description}</p>
-              <p>fait ?</p>
+    <div className="Main">
+        <main>
+            <div className="mes_listes">
+                <div class="select">
+                    <select>
+                        <option>Choisissez votre liste</option>
+                        <option>Liste 1</option>
+                        <option>Liste 2</option>
+                        <option>Ajouter une liste</option>
+                    </select>{' '}
+                    {/*pas possible de mettre un bouton dans une option
+      faut créer soi-meme un select */}
+                </div>
+                {/*petit bouton pour renommer ou supprimer la liste a coté de sa selection */}
+            </div>{' '}
+            {/*mettre une sorte de menu déroulant ou on peut choisir differentes listes*/}
+            <div className="Tasks">
+              {TaskData.map((task, index) => (
+                <Task
+                    key={index}
+                    task={task}
+                    index={index}
+                    handleTaskSelection={handleTaskSelection}
+                    taskRef={taskRefs}
+                />
+              ))}
             </div>
-          ))}
-        </div>
-      </main>
+        </main>
     </div>
-  )
-}
+);
+};
 
 export default Main
--- src/pages/Home/home.test.jsx
+++ src/pages/Home/home.test.jsx
@@ -1,18 +1,53 @@
 import React, {useState, useEffect} from 'react'
-import Main from './Main/Main.test'
+import Main from './Main/Main.jsx'
 import Aside from './Aside'
 
 const Home = ()  => {
-  const [tasks, setTasks] = useState([])
+    // Récupération des données dans la nouvelle Task
+    const [useTasks, setUseTasks] = useState(JSON.parse(localStorage.getItem('tasks')) ?? [])
 
-  const handleNewTask = (taskData) => {
-    setTasks([...tasks, taskData]) // contient task.titre, task.description, task.id
-    console.log(tasks)
-  }
+
+    const handleNewTask = (TaskData) => {
+        setUseTasks([...useTasks, TaskData])
+        console.log(useTasks)
+    }
+
+    useEffect(() => {
+      localStorage.setItem('tasks', JSON.stringify(useTasks));
+    }, [useTasks])
+
+    // Récupération de la Task séléctionnée
+    const [useTaskId, setUseTaskId] = useState(null)
+
+    const handleTaskId = (SelectTask) => {
+        setUseTaskId(SelectTask)
+    }
+
+
+    // Changement de classe de la Task selectionnée  |
+    // eslint-disable-next-line react-hooks/exhaustive-deps 
+    useEffect(() => {useTaskId && ChangeSelectedTaskClass()}, [useTaskId])
+
+    const ChangeSelectedTaskClass = () => {
+        useTaskId.classList.add('task-selected')
+    }
+
+
+    // Supprimer la Task
+    const handleDeleteTask = () => {
+        const updatedTasks = useTasks.filter(task => task !== useTaskId);
+        setUseTasks(updatedTasks);
+        setUseTaskId(null);
+        console.log(updatedTasks)
+    }
+// probleme de latence entre moment ou on click et le changement d'état de useDelTask
+// probleme quand on supprime la task : l'id n'est pas actualisé (si on suppirme )
+
+
     return (
         <div className='home'>
-            <Main tasks = {tasks}/>
-            <Aside addTask={handleNewTask}/>
+            <Main TaskData = {useTasks} SelectTask={handleTaskId}/>
+            <Aside addTask={handleNewTask} delTask={handleDeleteTask}/>
         </div>
     )
 }
